425795ae9553d6b9c26cb2a8f63eddb54256aec1
utthapon <utthapon.po@gmail.com>
2015-10-19 13:03:33 +0700

    เริ่มใช้ Github

---
+++ app/src/main/java/baac/po/utthapon/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,19 @@
+package baac.po.utthapon.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by BAAC on 19/10/2015.
+ */
+public class MyOpenHelper {
+    // explicit ประกาศตัวแปร
+
+    private static final String DATABASE_NAME = "BAAC.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+
+
+    public MyOpenHelper(Context context) {
+    }// สร้าง constructor ค่าเริ่มต้นเร่มทำงาน
+}   // Main Class คลาส หลัก
